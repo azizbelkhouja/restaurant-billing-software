b8a5d1bdc6b1f1977ae3bba174a6e6d76c723813
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-08-06 09:18:11 +0100

    done

--- main.c
+++ main.c
@@ -2,14 +2,60 @@
 #include <stdlib.h>
 #include <string.h>
 
-void bill(char name[50], char date[40]);
+void billDesign(char name[50], char date[40]);
+void printBill(char item[50], int qty, float price);
+void generateBill(float total);
+
+struct restaurant
+{
+    char name[40];
+    char item[50];
+    int noOfItems;
+    int chargePerItem;
+    int qty;
+    float totPrice;
+};
 
 int main()
 {
-    bill("aziz", "12/12/2020");
+    float total;
+    int ord;
+    struct restaurant r1[40];
+    printf("\n\t\t********** Unife Restaurant **********\n\n");
+    printf("Enter the customer name: ");
+    scanf("%s", r1->name);
+    printf("Enter the number of orders: ");
+    scanf("%d", &ord);
+
+    for(int i = 0; i < ord; i++)
+    {
+        printf("\nEnter item %d: ", i + 1);
+        scanf("%s", r1[i].item);
+        printf("Enter quantity of %s: ", r1[i].item);
+        scanf("%d", &r1[i].noOfItems);
+        printf("Enter the charge per item: ");
+        scanf("%d", &r1[i].chargePerItem);
+
+        total += r1[i].noOfItems * r1[i].chargePerItem;
+    }
+
+    system("cls");
+
+    billDesign(r1->name, "08/06/2024");
+
+    for(int i = 0; i < r1->noOfItems; i++)
+    {
+        printBill(r1[i].item, r1[i].noOfItems, r1[i].chargePerItem);
+    }
+    generateBill(total);
+
+    printf("\n\n");
+
+    return 0;
+
 }
 
-void bill(char name[50], char date[40])
+void billDesign(char name[50], char date[40])
 {
     printf("\n********** Unife Restaurant **********\n");
     printf("--------------------------------------\n");
@@ -18,5 +64,23 @@ void bill(char name[50], char date[40])
     printf("--------------------------------------\n");
     printf("Items\t\tQty\tPrice\tTotal\n\n");
     printf("--------------------------------------\n");
-
 }
+
+void printBill(char item[50], int qty, float price)
+{
+    printf("%s\t", item);
+    printf("\t%d", qty);
+    printf("\t%.2f", price);
+    printf("\t%.2f\t", qty*price);
+    printf("\n");
+}
+
+void generateBill(float total)
+{
+    printf("\n");
+    float iva = 0.07 * total;
+    printf("--------------------------------------\n");
+    printf("TOTALE COMPLESSIVO\t %0.2f", total);
+    printf("\nDi cui IVA\t\t %0.2f", iva);
+}
+
